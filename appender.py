d14098d01f4fec39a19be80507d339f8eec420cf
Морозова Екатерина <ekaterina.morozova@inside.glowbyte.com>
2023-06-27 14:38:53 +0300

    add list mode

--- appender.py
+++ appender.py
@@ -10,6 +10,10 @@ if (sys.argv[1]=='n'):
 	data["nodes"].append({"id":len(data["nodes"]),"label":sys.argv[2],"link":sys.argv[3]})
 if (sys.argv[1]=='l'):
 	data["links"].append({"source":int(sys.argv[2]),"target":int(sys.argv[3])})
+if (sys.argv[1]=='a'): #new author
+	data["authors"].append({"id":len(data["authors"]),"name":sys.argv[2],"link":sys.argv[3]})
+if (sys.argv[1]=='aa'): #add author 2 to node 3
+	data["nodes"][int(sys.argv[3])]["author"]=int(sys.argv[2])
 
 with open('json.json', 'w', encoding="utf-8") as outfile:
     json.dump(data, outfile, ensure_ascii=False, indent=4)
